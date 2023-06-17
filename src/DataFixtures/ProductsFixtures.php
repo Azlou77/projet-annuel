7efd1ac8fba9d7d91bcb815422379328cd3683bf
Nguyen Louis <nguyenlouis172@gmail.com>
2023-06-17 20:35:08 +0200

    add Trait for slugs

--- src/DataFixtures/ProductsFixtures.php
+++ src/DataFixtures/ProductsFixtures.php
@@ -4,14 +4,27 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Products;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Faker;
 
 class ProductsFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+          // use the factory to create a Faker\Generator instance
+          $faker = Faker\Factory::create('fr_FR');
 
-        $manager->flush();
+          for($prod = 1; $prod <= 10; $prod++){
+              $product = new Products();
+              $product->setName($faker->text(15));
+              $product->setDescription($faker->text());
+              $product->setSlug($this->slugger->slug($product->getName())->lower());
+              $product->setPrice($faker->numberBetween(900, 150000));
+              $product->setQuantity($faker->numberBetween(0, 10));
+
+            $manager->persist($product);
+            }
+            $manager->flush();
     }
 }
