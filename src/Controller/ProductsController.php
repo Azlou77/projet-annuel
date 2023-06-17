f4ae9bd521c33a7683ebd0532b0858cf9822c467
Nguyen Louis <nguyenlouis172@gmail.com>
2023-06-17 20:04:29 +0200

    add Datafixtures

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Controller;
-use App\Entity\Products;
 
+use App\Entity\Products;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductsController extends AbstractController
 {
+    // Display the list of products
     #[Route('/products', name: 'app_products')]
     public function index(): Response
     {
@@ -16,6 +17,8 @@ class ProductsController extends AbstractController
             'controller_name' => 'ProductsController',
         ]);
     }
+
+    // Display the details of a product
     #[Route('/{slug}', name: 'details')]
     public function details(Products $product): Response
     {
