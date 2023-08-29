3d6d6ac9c0f238007d4d0e2c0a5a5a74c759eaa5
Nguyen Louis <nguyenlouis172@gmail.com>
2023-08-29 10:56:53 +0200

    add files repository Fixes #23

--- src/Controller/FilesController.php
+++ src/Controller/FilesController.php
@@ -4,16 +4,15 @@
 namespace App\Controller; 
 
 // use generals 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
-use Symfony\Component\HttpFoundation\Response; 
-use Symfony\Compnent\Routing\Annotation\Route; 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;use Symfony\Component\HttpFoundation\Response; 
+use Symfony\Component\Routing\Annotation\Route; 
 use Symfony\Component\HttpFoundation\Request; 
 use Symfony\Component\String\Slugger\SluggerInterface; 
  
 // use for UploadedFile 
 use Symfony\Component\HttpFoundation\File\Exception\FileException; 
 use Symfony\Component\HttpFoundation\File\UploadedFile; 
-use Symfony\Component\HttpFoundation\File\File; 
+
 
 // use for Files 
 use App\Entity\Files; 
