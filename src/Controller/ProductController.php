4f644c7b53ea90ce5bb71c511119c93d5593e018
Nguyen Louis <nguyenlouis172@gmail.com>
2023-09-05 11:58:06 +0200

    add product template

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,10 +6,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 // Product 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use App\Form\ProductType;
 
 
 /**
@@ -23,24 +25,56 @@ class ProductController extends AbstractController
     /**
      * Function to display the list of Product
      * Route to display the list of Product
-     * @Route("/Product", name="app_Product_index")
+     * @Route("/products", name="app_products_index")
      * @return Response
      */
     public function index(ProductRepository $productRepository): Response
     {
         // Get all Product
-        $Product = $productRepository->findAll();
+        $product = $productRepository->findAll();
 
         // Return the view
-        return $this->render('Product/index.html.twig', compact('Product'));
+        return $this->render('product/index.html.twig', compact('product'));
     }
+
+    /**
+     * Function to add new product
+     * @Route("/product/new", name="app_product_new")
+     * @return Response
+     */
+
+    
+    public function new(Request $request, ProductRepository $productRepository): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        $user = $this->getUser();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productRepository->save($product, true);
+
+            return $this->redirectToRoute('app_product_index');
+        }
+
+        return $this->render('product/new.html.twig', [
+            'form' => $form->createView(),
+            'product' => $product,
+            'user' => $user
+        ]);
+        
+    
+     }
+
+
 
     /**
      * Function to display the details of a product
      * @Route("Product/{slug}", name="app_Product_details") ex: /Product/iphone-12
      * @return  Response
      */
-    #[Route('/Product/{slug}', name: 'app_details')]
+    #[Route('/product/{slug}', name: 'app_details')]
     public function details(Product $product): Response
     {
         // Return the view
